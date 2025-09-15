4c0697f3462e9fd01754024f7708ef39df713840
Sameh <samehnageh282@gmail.com>
2025-09-15 18:38:56 +0300

    signup controller add

---
+++ backend/lib/utils.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, //ms
+    httpOnly: true, // prevent xss :cross site scriptting
+    sameSite: "strict", // CREF attacks
+    secure: process.env.NODE_ENV === "development" ? false : true,
+  });
+  return token;
+};
