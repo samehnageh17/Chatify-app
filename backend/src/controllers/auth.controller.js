d96ce9c16eadbf2b8c863a4de2bb3b7156c70cc8
Sameh <samehnageh282@gmail.com>
2025-09-16 17:31:15 +0300

    login&logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -59,3 +59,33 @@ export const signup = async (req, res) => {
     });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "invalid credintals" });
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "invalid credintals" });
+
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (err) {
+    console.error("Error in Login ", err);
+    res.status(500).json({
+      message: "intrenal server error",
+    });
+  }
+};
+export const logout = async (req, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "loged out succeffuly" });
+};
