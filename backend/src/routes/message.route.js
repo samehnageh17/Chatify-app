af78c0453e7ba7b8a7484c69a56bed25cb0a2410
Sameh <samehnageh282@gmail.com>
2025-09-17 18:28:34 +0300

    message route added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,5 +1,19 @@
 import express from "express";
+import {
+  getAllContacts,
+  getMassegesByUserId,
+  sendMessagesByUserID,
+  getChatPartners,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjectProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+router.use(protectRoute);
+// router.use(arcjectProtection,protectRoute);
+router.get("/contacts", getAllContacts);
+router.get("/:id", getMassegesByUserId);
+router.get("/chats", getChatPartners);
+router.post("/send/:id", sendMessagesByUserID);
 
 export default router;
